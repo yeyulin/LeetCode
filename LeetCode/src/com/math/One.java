21a6dea82e02e0d32c23032f8ee32fafa5354c38
yeyulin <ityeyulin@163.com>
2018-08-13 19:13:47 +0800

    easy math

---
+++ LeetCode/src/com/math/One.java
@@ -0,0 +1,29 @@
+package com.math;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Fizz Buzz
+ */
+public class One {
+    public List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>(n);
+        for (int i = 1; i <= n; i++) {
+            if(i % 3 == 0 && i % 5 == 0){
+                result.add("FizzBuzz");
+                continue;
+            }
+            if (i % 3 == 0) {
+                result.add("Fizz");
+                continue;
+            }
+            if (i % 5 == 0) {
+                result.add("Buzz");
+                continue;
+            }
+            result.add(i+"");
+        }
+        return result;
+    }
+}
